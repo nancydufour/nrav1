6ca918d7f0fd789896a89b00e24c7c760571ddcc
Oluwapelumi <pelekadeba@gmail.com>
2026-08-13 02:59:06 +0100

    Paystack Integration #4 - Disabled Paystack Button

--- src/pages/DonateOptions.tsx
+++ src/pages/DonateOptions.tsx
@@ -519,7 +519,8 @@ const DonateOptions: React.FC<DonateOptionsProps> = ({ onBack }) => {
               type="button"
               onClick={handleDonate}
               disabled={
-                getCurrentAmount() < 100 || isProcessing
+                // getCurrentAmount() < 100 || isProcessing
+                true
               }
               className={`inline-flex items-center justify-center gap-3 rounded-full px-12 py-4 font-montserrat text-xl font-bold shadow-lg transition-all duration-300 ${
                 getCurrentAmount() >= 100 && !isProcessing
